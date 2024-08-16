7f3428b30632192190d411d4e1f439b4f7e5cbc0
@hexlet/cli <support@hexlet.io>
2024-08-16 15:57:29 +0300

    submit java-spring-ru/jpa-repository

---
+++ java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package exercise.repository;
+
+import java.util.List;
+import exercise.model.Product;
+
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT e FROM Product e WHERE e.price BETWEEN :min AND :max ORDER BY e.price ASC")
+    public List<Product> findByPriceRange(
+        @Param("min") Integer min,
+        @Param("max") Integer max);
+}
